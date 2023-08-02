c52a39d5f36bc21269da5e3e02bac60e0abf0997
mongdie <mongdie06240624@126.com>
2023-08-02 15:38:43 +0800

    更改商品某二级分类下的商品列表分页展示

--- pipayshop-api/src/main/java/com/example/pipayshopapi/mapper/ItemCommodityInfoMapper.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/mapper/ItemCommodityInfoMapper.java
@@ -22,9 +22,9 @@ import java.util.List;
 @Mapper
 public interface ItemCommodityInfoMapper extends BaseMapper<ItemCommodityInfo> {
 
-    List<CommodityVO> commodityOfCateList(@Param("categoryId") Integer categoryId, @Param("startIndex")Integer startIndex, @Param("number")Integer number);
+    List<CommodityVO> commodityOfCateList(@Param("categoryPid") Integer categoryPid, @Param("startIndex")Integer startIndex, @Param("number")Integer number);
 
-    Integer  listCount(@Param("categoryId") Integer categoryId);
+    Integer  listCount(@Param("categoryPid") Integer categoryPid);
 
     List<CommodityVO> itemCommodityChoose(String itemId, String brandId);
 
