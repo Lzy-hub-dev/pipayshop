2415d53ffd63d3791b989bce97cf9d922e68232a
sms <491579134@qq.com>
2023-08-05 16:56:55 +0800

    根據商店id查商品信息

--- pipayshop-api/src/main/java/com/example/pipayshopapi/entity/dto/ShopCommodityLiveInfoListDTO.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/entity/dto/ShopCommodityLiveInfoListDTO.java
@@ -1,8 +1,10 @@
 package com.example.pipayshopapi.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
@@ -17,6 +19,12 @@ public class ShopCommodityLiveInfoListDTO {
 
 
    private String shopId;
+
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
    private Date startTime;
+
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
    private Date endTime;
 }
