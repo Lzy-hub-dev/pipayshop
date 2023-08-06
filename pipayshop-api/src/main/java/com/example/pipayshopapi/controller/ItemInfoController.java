442a779a9c9ead1153e73a578fc5a440685549c8
sms <491579134@qq.com>
2023-08-06 18:13:42 +0800

    item_info表新增upload_balance字段 根据item_id 查询 商店剩余上传数

--- pipayshop-api/src/main/java/com/example/pipayshopapi/controller/ItemInfoController.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/controller/ItemInfoController.java
@@ -35,13 +35,13 @@ public class ItemInfoController {
 
     @GetMapping("getUploadBalance/{itemId}")
     @ApiOperation("根据网店id获取商品上架剩余数")
-    public ResponseVO<Integer> getUploadBalance(String itemId){
+    public ResponseVO getUploadBalance(String itemId){
         try {
             Integer uploadBalance = itemInfoMapper.getUploadBalance(itemId);
             if (uploadBalance == null) {
                 throw new Exception();
             }
-            return ResponseVO.getSuccessResponseVo(uploadBalance);
+            return ResponseVO.getSuccessResponseVo(uploadBalance > 0);
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new BusinessException("获取商品上架剩余数失败，请联系后台人员");
