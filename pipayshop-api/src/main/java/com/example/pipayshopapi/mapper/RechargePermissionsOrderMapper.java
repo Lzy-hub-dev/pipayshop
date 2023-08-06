1b938e6b54d4c431b42686a92eb3af69c14256a4
mongdie <mongdie06240624@126.com>
2023-08-06 18:53:07 +0800

    加

---
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/mapper/RechargePermissionsOrderMapper.java
@@ -0,0 +1,18 @@
+package com.example.pipayshopapi.mapper;
+
+import com.example.pipayshopapi.entity.RechargePermissionsOrder;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zxb
+ * @since 2023-08-06
+ */
+@Mapper
+public interface RechargePermissionsOrderMapper extends BaseMapper<RechargePermissionsOrder> {
+
+}
