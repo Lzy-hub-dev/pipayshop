3495d5169fdab1e0e8fb5fbeef97762e332a2e71
wzx <2660357088@qq.com>
2023-08-06 18:52:13 +0800

    网店商品订单的规格详情

--- pipayshop-api/src/main/java/com/example/pipayshopapi/entity/ItemOrderInfo.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/entity/ItemOrderInfo.java
@@ -9,7 +9,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -87,4 +86,8 @@ public class ItemOrderInfo implements Serializable {
      */
     private Integer number;
 
+    /**
+     *商品的规格
+     */
+    private String commoditySpecification;
 }
