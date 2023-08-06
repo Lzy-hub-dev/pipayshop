04a62e193981f5b66ad8db1f0c5aa4e9721bcf25
sms <491579134@qq.com>
2023-08-06 15:58:46 +0800

    根据buyerDataId修改默认地址标志

--- pipayshop-api/src/main/java/com/example/pipayshopapi/mapper/BuyerDataMapper.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/mapper/BuyerDataMapper.java
@@ -3,6 +3,8 @@ package com.example.pipayshopapi.mapper;
 import com.example.pipayshopapi.entity.BuyerData;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * <p>
@@ -15,4 +17,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface BuyerDataMapper extends BaseMapper<BuyerData> {
 
+
+    @Select("select user_id from buyer_data where buyer_data_id = #{buyerDataId}")
+    String getUserId(@Param("buyerDataId") String buyerDataId);
 }
