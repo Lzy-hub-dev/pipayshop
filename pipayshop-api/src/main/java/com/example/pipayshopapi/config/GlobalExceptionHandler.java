e0ef59fcdc91e7df5c20da1f3a1d0ca396d188e6
jiangjiafeng <779666271@qq.com>
2023-08-07 19:56:10 +0800

    全局异常

---
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/config/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.pipayshopapi.config;
+
+import com.example.pipayshopapi.entity.vo.ResponseVO;
+import com.example.pipayshopapi.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author: jjf
+ * @ClassName GlobalExceptionHandler
+ * @Description 全局异常处理
+ * @date 2023/8/7 19:30
+ * @Version 1.0
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseVO handleException(BusinessException e) {
+        e.printStackTrace();
+        return ResponseVO.getFalseResponseMsg(e.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseVO baseException(BusinessException e) {
+        e.printStackTrace();
+        return ResponseVO.getFalseResponseMsg(e.getMessage());
+    }
+}
