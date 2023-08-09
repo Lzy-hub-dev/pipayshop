79fe9f282e075d4bdfae7fc27c873811f60f2fad
sms <491579134@qq.com>
2023-08-09 12:49:44 +0800

    酒店列表展示

---
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/entity/vo/HotelInfoVO.java
@@ -0,0 +1,26 @@
+package com.example.pipayshopapi.entity.vo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+/**
+ * fileName: HotelInfoVO
+ * author: 四面神
+ * createTime:2023/8/9 11:50
+ * 描述:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HotelInfoVO {
+
+    private String shopId;
+    private String shopName;
+    private String tagList;
+    private String address;
+    private Double score;
+    private String shopImagList;
+    private String shopIntroduce;
+    private String userImage;
+}
