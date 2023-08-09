a31aadcc93a20c0733265fe187ad8cb34646ee9c
jiangjiafeng <779666271@qq.com>
2023-08-09 22:41:11 +0800

    超时订单。rabbitmq处理

--- pipayshop-api/src/main/java/com/example/pipayshopapi/PipayshopApiApplication.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/PipayshopApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.pipayshopapi;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableRabbit
 public class PipayshopApiApplication {
 
     public static void main(String[] args) {
