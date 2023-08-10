dc9fec6ff0f5529e339e79d2221ebd9ee414afaa
mongdie <mongdie06240624@126.com>
2023-08-10 14:22:16 +0800

    酒店订单

--- pipayshop-api/src/main/java/com/example/pipayshopapi/entity/vo/ShopLiveOrderDetailVO.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/entity/vo/ShopLiveOrderDetailVO.java
@@ -40,13 +40,5 @@ public class ShopLiveOrderDetailVO {
 
     private String phone;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date startTime;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date endTime;
-
 
 }
