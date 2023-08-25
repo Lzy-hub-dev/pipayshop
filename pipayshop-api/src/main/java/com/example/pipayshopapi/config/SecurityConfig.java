85e3789459d8e66ac87ab4bd3d45215afa64b2ee
zhang <1216019850@qq.com>
2023-08-25 17:53:17 +0800

    优化放行websocker

--- pipayshop-api/src/main/java/com/example/pipayshopapi/config/SecurityConfig.java
+++ pipayshop-api/src/main/java/com/example/pipayshopapi/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/pipayshopapi/user-info/login").anonymous()
                 .antMatchers("/images/**").permitAll()
+                .antMatchers("/dailyActive/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
         //把token校验过滤器添加到过滤器链中
